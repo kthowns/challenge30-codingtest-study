87c1d83fd5a5be29d6534c0c752ffa786feac88f
notenoughnowjy <jyp4188@gmail.com>
2023-05-01 00:15:04 +0900

    [CDT] repeatshootingthestarsfivetimes / Lv2 / 2분
    https://www.codetree.ai/missions/5/problems/repeat-shooting-the-stars-five-times/description

---
+++ 박준영-notenoughnowjy/Lv2/repeatshootingthestarsfivetimes.java
@@ -0,0 +1,11 @@
+package Lv2;
+
+public class repeatshootingthestarsfivetimes{
+    public static void main(String args[]){
+
+        for(int i=0; i<5; i++){
+            System.out.println("**********");
+        }
+
+    }
+}
