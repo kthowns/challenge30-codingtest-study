5f07042d79b399046fb1f15a45f6ebe00bfda30c
Laptop-Limjihoon <lonelynight1026@gmail.com>
2023-05-01 19:03:14 +0900

    [CDT] Call By Value & Call By Reference / Lv2 / 10분
    https://www.codetree.ai/missions/5/problems/operational-values-for-two-integers-2/description

--- 임지훈-vanillacake369/Lv2/CallBy.java
+++ 임지훈-vanillacake369/Lv2/CallBy.java
@@ -8,14 +8,23 @@ public class CallBy {
         int a = Integer.parseInt(inputStr[0]);
         int b = Integer.parseInt(inputStr[1]);
         /* calc */
-        /* result */
         int[] result = computeTwoInt(a, b);
+        /* result */
         System.out.println((result[0]) + " " + (result[1]));
     }
 
+    /**
+     * 의사코드
+     * 1. 두 개짜리의 배열 생성
+     * 2. 입력 a,b 비교
+     * 2-1. a가 크다면 :: 배열의 0 번째에 a*2, 1번째에 b+10 저장
+     * 2-2. b가 크다면 :: 배열의 0 번째에 a+10, 1번째에 b*2 저장
+     * 3. 배열 반환
+     */
     private static int[] computeTwoInt(int a, int b) {
         int[] temp = new int[2];
         if (a > b) {
+            temp[0] = a * 2;
             temp[1] = b + 10;
         } else {
             temp[0] = a + 10;
