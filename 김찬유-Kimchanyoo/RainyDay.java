8d311a751bdb212f941b3a7998855c45b498175c
Chanyoo Kim <cksdb012@gmail.com>
2023-05-11 00:39:18 +0900

    [CDT] 비오는 날 / Lv2 / 1시간
    https://www.codetree.ai/missions/5/problems/rainy-day/submissions

--- 김찬유-Kimchanyoo/RainyDay.java
+++ 김찬유-Kimchanyoo/RainyDay.java
@@ -8,6 +8,41 @@ import java.util.Scanner;
 3. 비오는 날이 들어가있는 리스트를 정렬한다
 4. 리스트가 정렬되어져있기 때문에 리스트의 0번째 값을 출력한다
  */
+public class RainyDay {
+    static ArrayList<Day> rainyDayList = new ArrayList<Day>();
+    static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        int n = sc.nextInt();
+        sc.nextLine();
+        storeRainyDayList(n);
+        sortedDayList();
+        printEarlyRainyDay();
+    }
+
+    /**
+     * 기상 정보를 입력받고 날씨가 Rain일시 rainyDayList에 집어넣는 함수
+     */
+    private static void storeRainyDayList(int n) {
+        for(int i = 0; i < n; i++){
+           String[] tempList = sc.nextLine().split(" ");
+           if(tempList[2].equals("Rain")){
+               rainyDayList.add(new Day(tempList[0],tempList[1],tempList[2]));
+           }
+        }
+    }
+    /**
+     * 비오는 날이 들어가있는 리스트를 정렬하는 함수
+     */
+    private static void sortedDayList() {
+        Collections.sort(rainyDayList);
+    }
+    /**
+     * 비오는 날을 출력하는 함수
+     */
+    private static void printEarlyRainyDay() {
+        System.out.println(rainyDayList.get(0).toString());
+    }
+}
 class Day implements Comparable<Day>{
     String day;
     String dayOfWeek;
@@ -45,39 +80,3 @@ class Day implements Comparable<Day>{
         return this.day.compareTo(date.day);
     }
 }
-public class RainyDay {
-    static ArrayList<Day> rainyDayList = new ArrayList<Day>();
-    static Scanner sc = new Scanner(System.in);
-    public static void main(String[] args) {
-        int n = sc.nextInt();
-        sc.nextLine();
-        storeRainyDayList(n);
-        sortedDayList();
-        printEarlyRainyDay();
-    }
-
-    /**
-     * 기상 정보를 입력받고 날씨가 Rain일시 rainyDayList에 집어넣는 함수
-     */
-    private static void storeRainyDayList(int n) {
-        for(int i = 0; i < n; i++){
-           String[] tempList = sc.nextLine().split(" ");
-           if(tempList[2].equals("Rain")){
-               rainyDayList.add(new Day(tempList[0],tempList[1],tempList[2]));
-           }
-        }
-    }
-    /**
-     * 비오는 날이 들어가있는 리스트를 정렬하는 함수
-     */
-    private static void sortedDayList() {
-        Collections.sort(rainyDayList);
-    }
-    /**
-     * 비오는 날을 출력하는 함수
-     */
-    private static void printEarlyRainyDay() {
-        System.out.println(rainyDayList.get(0).toString());
-    }
-}
-
