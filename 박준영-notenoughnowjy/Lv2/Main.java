1a8696b3eeca865f3ba9e863e8f550ede4b40002
notenoughnowjy <jyp4188@gmail.com>
2023-05-23 08:20:22 +0900

    [CDT] / 물체 단위로 완전탐색 / 좌표평면 위의 특정 구역 2 / Lv2 / 50분

--- 박준영-notenoughnowjy/Lv2/Main.java
+++ 박준영-notenoughnowjy/Lv2/Main.java
@@ -1,22 +1,29 @@
 public class Main{
-
-  public static int[] numbers = new int[]{1,5,3,6};
+  public static int n = 5;
+  public static int[][] arr = new int[][]{
+    {1,3}, {2,4}, {5,8}, {6,9}, {7,10}
+  };
   public static void main(String[] args){
-    int mindiff = 100;
-    for(int i=0;i<4;i++){
-      for(int j=i+1;j<4;j++){
-        mindiff = Math.min(mindiff, getDiff(i,j));
+    int result = Integer.MAX_VALUE;
+    for(int i=0; i<n; i++){
+      int[] counting = new int[11];
+      for(int j=0; j<n; j++){
+        if(i==j){
+          continue;
+        }
+        int x1 = arr[j][0], x2 = arr[j][1];
+        for(int k=x1; k<=x2; k++){
+          counting[k]++;
+        }
       }
+      
+      int maxCnt = 0;
+      for(int j=0; j<11; j++){
+        maxCnt = Math.max(maxCnt, counting[j]);
+      }
+
+      result = Math.min(result, maxCnt);
     }
-    System.out.println(mindiff);
-  }
-  private static int getDiff(int i, int j) {
-    int sum1 = numbers[i] + numbers[j];
-    int sum2 = 0;
-    for(int k=0; k<4; k++){
-      sum2 += numbers[k];
-    }
-    sum2 -= sum1;
-    return Math.abs(sum1 - sum2);
+    System.out.println(result);
   }
 }
