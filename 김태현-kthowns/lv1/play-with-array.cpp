9596d43fadc2572626b91e842e18bf3adeda714a
kthowns <70064360+kthowns@users.noreply.github.com>
2023-05-26 16:30:20 +0900

    [CDT] 배열놀이 / lv1 / 19분
    https://www.codetree.ai/missions/4/problems/play-with-array/submissions

---
+++ 김태현-kthowns/lv1/play-with-array.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+int main() {
+    int n, qType, q;
+    int a, b;
+
+    std::cin >> n >> q;
+
+    int el[n] = {0,};
+
+    for(int i=0; i<n; i++){
+        int tmp;
+        std::cin >> tmp;
+        el[i] = tmp;
+    }
+    
+    for(int i=0; i<q; i++){
+        a = 0;
+        b = 0;
+        std::cin >> qType;
+        if(qType == 1){
+            std::cin >> a;
+            std::cout << el[a-1];
+        }else if(qType == 2){
+            int it = 0;
+            std::cin >> a;
+            for(int j=0; j<n; j++){
+                if(el[j] == a){
+                    it = j+1;
+                    break;
+                }
+            }
+            std::cout << it;
+        }else if(qType == 3){
+            std::cin >> a >> b;
+            for(int i=a-1; i<=b-1; i++)
+                std::cout << el[i] << ' ';
+        }
+        std::cout << std::endl;
+    }
+    return 0;
+}
