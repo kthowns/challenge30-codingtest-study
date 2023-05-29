084af4c3f66050c9484d62faa53166481432aaef
notenoughnowjy <jyp4188@gmail.com>
2023-05-29 16:26:01 +0900

    [CDT] / 구간 단위로 완전탐색 / Lv2 / 60분

--- 박준영-notenoughnowjy/Lv2/GorH2.java
+++ 박준영-notenoughnowjy/Lv2/GorH2.java
@@ -10,22 +10,80 @@
  * 입력 : 
  * 6
  * 4 G
+ * 10 H
+ * 7 G
+ * 16 G
+ * 1 G
+ * 3 H
+ * 
+ * 출력 :
+ * 7
+ * 
+ * 입력 : 
+ * 1
+ * 2 G
+ * 
+ * 출력 : 
+ * 0
  */
+
+
+ /*
+  * 의사 코드
+  1. n값을 입력받는다.
+  2. char값을 받을 arr배열을 만든다.
+  3. x의 값을 이용해서 char c 의 문자를 위치에 입력받는다.
+  4. 각 위치에서 값을 비교한다.
+  5. g의 개수와 h의 개수를 각각 헤아린다.
+  6. g의 개수가 0이거나 h의 개수가 0이거나 g-h의 값이 0일 경우 j-i값을 한다.
+  7. len의 최대값들을 뽑아서 maxLen에 넣어준다.
+  8. maxLen을 출력한다.
+  */
 
 import java.util.Scanner;
 
 public class GorH2{
-  public static int Max_N = 100;
+  public static int Max_N = 101;
   public static void main(String[] args){
     Scanner sc = new Scanner(System.in);
 
     int n = sc.nextInt();
-    int[] positions = new int[Max_N];
-    String[] strings = new String[Max_N];
-    
+    char[] arr = new char[Max_N];
+
     for(int i=0; i<n;i++){
-      positions[i] = sc.nextInt();
-      strings[i] = sc.next().charAt(0);
+      int x = sc.nextInt();
+      char c = sc.next().charAt(0);
+
+      arr[x] = c;
     }
+
+    int maxLen = 0;
+    for(int i=0; i<Max_N; i++){
+      for(int j=i+1; j<Max_N; j++){
+        if(arr[i] == 0 || arr[j] == 0){
+          continue;
+        }
+        int Gcnt = 0;
+        int Hcnt = 0;
+
+        for(int k=i;k<=j;k++){
+          if(arr[k] == 'H'){
+            Hcnt++;
+          }
+          if(arr[k] == 'G'){
+            Gcnt++;
+          }
+        }
+
+        if(Hcnt == 0 || Gcnt == 0 || Hcnt == Gcnt){
+          int len = j - i;
+          maxLen = Math.max(maxLen, len);
+        }
+      }
+    }
+    System.out.println(maxLen);
+    // for(int i=0;i<Max_N;i++){
+    //   System.out.println(i + ":" + arr[i]);
+    // }
   }
 }
